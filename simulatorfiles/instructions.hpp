02f5c3e03fb49169e79cee7211ae6d20aa08aad9
Seb <seb.fenton@gmail.com>
2018-10-17 22:41:52 +0100

    Register instructions added

--- simulatorfiles/instructions.hpp
+++ simulatorfiles/instructions.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
-int instruction1();
+class simulator_registers{
+    public:
+        simulator_registers();
+        char get_register(int address);
+        void set_register(char input, int address);
+
+    private:
+        char registers [31];
+};
+
+class simulator_memory{
+
+};
