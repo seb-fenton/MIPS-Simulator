a1e2386ffbec142d8250be032532106e70e3dce7
Seb <seb.fenton@gmail.com>
2018-10-20 13:42:59 +0100

    changed test file directories

---
+++ test/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###INITIALISATION OF TEST DIRECTORY AND FILES###
+echo "--#Initialising test directory"
+#mkdir -p bin/test
+touch test/output.csv
+touch test/temp.bin
+printf "TestId\tInstruction\tStatus\tAuthor\tMessage\n" >> bin/test/testoutput.csv
+
+###TAKES FILE INPUT###                                           
+my_function() {
+    echo "--#Stored filename argument:"
+    for arg in "${commandline_args[@]}"; do
+        echo "$arg"
+    done
+}
+
+commandline_args=("$@")
+
+my_function
+
+###CREATES BINARY FILES AND CALLS THEM WITH SIMULATOR EXECUTABLE?, OUTPUTTING FORMAT BELOW TO CSV###
+
+#TestId , Instruction , Status , Author [, Message]
+
+
+
+
+
+
