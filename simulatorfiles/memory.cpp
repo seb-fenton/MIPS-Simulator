4542053ddd986e851f803b5e9f173c35af83a8a0
Seb <seb.fenton@gmail.com>
2018-10-21 15:36:39 +0100

    indentation fix

--- simulatorfiles/memory.cpp
+++ simulatorfiles/memory.cpp
@@ -31,71 +31,72 @@ void sim_reg::set_reg(char input, int address){
     }
 }
 
-    //SIMULATOR_MEMORY FUNCTION DEFINITIONS//
-    //constructor
-    sim_mem::sim_mem(){
-        //load binary into executable memory
+//SIMULATOR_MEMORY FUNCTION DEFINITIONS//
+//constructor
+sim_mem::sim_mem(){
+    //load binary into executable memory
 
-        //initialise data memory to zero
+    //initialise data memory to zero
+}
+
+//Checks for a valid address, then subtracts the starting address and maps the appropriate memory region
+//via the return character.
+char sim_mem::addressmap(int &address){
+    if(0 <= address && address < 4)
+        return 0; // 0 for addr
+    if (0x10000000 <= address && address < 0x11000000){
+        address = address - 0x10000000;
+        return 1; //1 for inst
     }
-
-    //Checks for a valid address, then subtracts the starting address and maps the appropriate memory region
-    //via the return character.
-    char sim_mem::addressmap(int &address){
-        if(0 <= address && address < 4)
-            return 0; // 0 for addr
-        if (0x10000000 <= address && address < 0x11000000){
-            address = address - 0x10000000;
-            return 1; //1 for inst
-        }
-        if (0x20000000 <= address && address < 0x24000000){
-            address = address - 0x20000000;
-            return 2; //2 for data
-        }  
-        if (0x30000000<= address && address < 0x30000004){
-            address = address - 0x30000000;
-            return 3; //3 for getc
-        }
-        if (0x30000004 <= address && address < 0x30000008){
-            address = address - 0x30000004;
-            return 4; //4 for putc
-        }
-        else return x;
+    if (0x20000000 <= address && address < 0x24000000){
+        address = address - 0x20000000;
+        return 2; //2 for data
+    }  
+    if (0x30000000<= address && address < 0x30000004){
+        address = address - 0x30000000;
+        return 3; //3 for getc
     }
+    if (0x30000004 <= address && address < 0x30000008){
+        address = address - 0x30000004;
+        return 4; //4 for putc
+    }
+    else return x;
+    //what is x? should we give it value -1?
+}
 
-    char sim_mem::get_byte(int address){
-        char check = sim_mem::addressmap(address);
+char sim_mem::get_byte(int address){
+    char check = sim_mem::addressmap(address);
         
-        /*Memory exceptions (-11): 
-        1. Reading from addr_null
-        2. Reading from write-only memory zone addr_putc
-        3. Address out of range or blank areas*/
-        if(check == '0' || check == '4' || check == 'x')
-            std::exit(-11);
+    /*Memory exceptions (-11): 
+    1. Reading from addr_null
+    2. Reading from write-only memory zone addr_putc
+    3. Address out of range or blank areas*/
+    if(check == '0' || check == '4' || check == 'x')
+        std::exit(-11);
 
-        //ACTUAL FUNCTION
-        //map numerical address to the correct array
-        switch(check){
-            case '1': return addr_instr[address];
-            case '2': return addr_data[address];
-            case '3': return addr_getc[address];
-        }
+    //ACTUAL FUNCTION
+    //map numerical address to the correct array
+    switch(check){
+        case '1': return addr_instr[address];
+        case '2': return addr_data[address];
+        case '3': return addr_getc[address];
     }
+}
 
 
-    void sim_mem::set_byte(int address, char value){
-        /*Memory exceptions (-11): 
-        1. Writing to addr_null
-        2. Writing to instruction Memory
-        3. Writing to write-only memory zone addr_putc
-        4. Address out of range or Blank Areas
-        */
-        if(check == '0' || check == '1' || check == '3' || check =='x')
-            std::exit(-11);
+void sim_mem::set_byte(int address, char value){
+    /*Memory exceptions (-11): 
+    1. Writing to addr_null
+    2. Writing to instruction Memory
+    3. Writing to write-only memory zone addr_putc
+    4. Address out of range or Blank Areas
+    */
+    if(check == '0' || check == '1' || check == '3' || check =='x')
+        std::exit(-11);
         
-       //Actual function
-       switch(check){
-            case '2': addr_data[address] = value;
-            case '4': addr_getc[address] = value;
-        }
+    //Actual function inserts value into address if no errors are thrown
+    switch(check){
+        case '2': addr_data[address] = value;
+        case '4': addr_getc[address] = value;
     }
+}
