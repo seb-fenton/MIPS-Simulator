8a846351b231848c10b7ecd019eecd70d6dc2d4b
Seb <seb.fenton@gmail.com>
2018-10-22 10:51:37 +0100

    elaboration on input binary processing in main

--- simulatorfiles/main.cpp
+++ simulatorfiles/main.cpp
@@ -4,11 +4,40 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 
 int main(int argc, char* argv[]){
     //INITIALISE MEMORY AND REGISTERS
     sim_mem MemModule;
     //LOAD BINARY INTO MEMORY
+
+    
+    //Defines string to hold the input binary
+    std::string InputBinaryFile;
+    bool BinaryInputTrueOrFalse;
+    //std::cout << argv[0] << std::endl;
+
+    //if argc holds more than main.cpp path
+    if (argc > 1) {
+        //if true, then let InputBinaryFile equal the first command line parameter
+        InputBinaryFile = argv[1];
+        BinaryInputTrueOrFalse = true;
+        //std::cout << InputBinaryFile << std::endl;
+    } else {
+        //ISSUE!!! 
+        //How do we handle having no binary input? does it just initialise and sit there on its own, or does the program exit?
+        std::cerr<<"ERROR: -01????"<<std::endl;
+        //std::exit(-11??);
+    }
+
+    std::vector<char> InputBinaryVector;
+
+    
+
+
+
+
+
 
     //BEGIN CONTROL LOOP WITH SIMULATOR OBJECT
         //Obtain instruction
@@ -17,19 +46,6 @@ int main(int argc, char* argv[]){
             //instruction does its thing
         //PC + 4 or branch adjustment
     
-   
-    /*
-    std::string fileName;
-    std::cout << argv[0] << std::endl;
-
-    if (argc > 1) {
-        fileName = argv[1];
-        std::cout << fileName << std::endl;
-    } else {
-        //ISSUE!!! 
-        std::cerr<<"ERROR: -01????"<<std::endl;
-        std::exit(0);
-    }*/
 
     return 0;
 }
